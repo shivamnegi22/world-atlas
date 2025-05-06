59b61b18c4755aaa6ce52e1d4ce6ebd4187b5719
unknown <shivamnegi2207@gmail.com>
2025-05-06 10:30:09 +0530

    vite.config.js configured

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/world-atlas/', // 👈 this fixes GitHub Pages path issue
 })
