e0eb71c3c00c8482ef81e219df4c05f1f551de8d
unknown <shivamnegi2207@gmail.com>
2025-05-06 11:43:57 +0530

    path fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,29 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { AppLayout } from "./components/Layout/AppLayout";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Country } from "./pages/Country";
 import { Contact } from "./pages/Contact";
-import { ErrorPage } from "./pages/ErrorPage";
 import { CountryDetails } from "./components/Layout/CountryDetails";
+import { Headers } from "./components/UI/Headers";
+import { Footers } from "./components/UI/Footer";
 
 // Create router with the basename for GitHub Pages
-const router = createBrowserRouter([
+const App =() =>
   {
-    path: "/",
-    element: <AppLayout />,
-    errorElement: <Home />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "country",
-        element: <Country />,
-      },
-      {
-        path: "country/:id",
-        element: <CountryDetails />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
-
-const App = () => {
-  return (
-    <RouterProvider router={router} />
-  );
-};
+    return (
+  <>
+  <Headers  />
+  <Routes>
+  <Route  path="/" element={<Home />} />
+  <Route  path="/about" element={<About />} />
+  <Route  path="/country" element={<Country />} />
+  <Route  path="/country/:id" element={<CountryDetails />} />
+  <Route  path="/contact" element={<Contact/>} />
+  </Routes> 
+  <Footers />     
+  </>
+    );
+  };
 
 export default App;
