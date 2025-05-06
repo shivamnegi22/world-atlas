4c14259060b87da6907ba56ab29ca96ae931933f
unknown <shivamnegi2207@gmail.com>
2025-05-06 12:51:53 +0530

    full fixed-2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Detect if in production mode
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 this fixes GitHub Pages path issue
+  base: isProd ? '/world-atlas/' : '/', // 👈 dynamic base path
 })
