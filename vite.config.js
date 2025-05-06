123d445cec7c8720c4b8ae2d94dd63fea55eae8f
unknown <shivamnegi2207@gmail.com>
2025-05-06 13:08:11 +0530

    full fixed-2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Detect if in production mode
-const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   plugins: [react()],
-  base: isProd ? '/world-atlas/' : '/', // 👈 dynamic base path
+  base:'/world-atlas',
 })
