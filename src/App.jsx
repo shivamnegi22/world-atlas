3e09e5e3480de9ec8f7f24e4ebb60390821d5948
unknown <shivamnegi2207@gmail.com>
2025-05-13 11:00:50 +0530

    default set

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom"; // ✅ Add Navigate
 import "./App.css";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
@@ -8,23 +8,21 @@ import { CountryDetails } from "./components/Layout/CountryDetails";
 import { Headers } from "./components/UI/Headers";
 import { Footers } from "./components/UI/Footer";
 
-// Create router with the basename for GitHub Pages
-const App =() =>
-  {
-    return (
-  <>
-  <Headers  />
-  <Routes>
-  <Route  path="/" element={<Home />} />
-  <Route  path="/about" element={<About />} />
-  <Route  path="/country" element={<Country />} />
-  <Route  path="/country/:id" element={<CountryDetails />} />
-  <Route  path="/contact" element={<Contact/>} />
-  </Routes> 
-  
-  <Footers />     
-  </>
-    );
-  };
+const App = () => {
+  return (
+    <>
+      <Headers />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/country" element={<Country />} />
+        <Route path="/country/:id" element={<CountryDetails />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      <Footers />
+    </>
+  );
+};
 
 export default App;
